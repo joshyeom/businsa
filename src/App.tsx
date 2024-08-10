c57d930fe8f90e71a92d39bc71f72f6ef632963d
joshyeom <jhyeom95@daum.net>
2024-08-10 19:25:01 +0900

    feat: AuthProvider: 역할 별 페이지 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main, Signin, Signup } from "./pages";
+import { AuthProvider } from "./contexts/AuthContext";
+
 
 const App = () => {
   return(
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,24 @@
-import { useRouteHandler } from "../hooks/routeHandler"
+import { useRouteHandler } from "../hooks/useRouteHandler";
+import { useAuth } from "../contexts/AuthContext";
 
 const Main = () => {
-    const route = useRouteHandler()
-   
+  const route = useRouteHandler();
+  const { userRole } = useAuth(); // AuthProvider로 감싸져 있어야 합니다.
 
-    return(
+  return (
+    <>
+      {!userRole ? (
         <>
-            <button onClick={() => route('signin')}>로그인</button>
-            <button onClick={() => route('signup')}>회원가입</button>
+          <button onClick={() => route('signin')}>로그인</button>
+          <button onClick={() => route('signup')}>회원가입</button>
         </>
-    )
+      ) : userRole === "buyer" ? (
+        <div>환영합니다 구매자님</div>
+      ) : (
+        <div>환영합니다 판매자님</div>
+      )}
+    </>
+  );
 }
 
-export default Main
+export default Main;
