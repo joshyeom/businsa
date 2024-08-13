49655a7ebd4c36a9075b90574b2a0136b042a3fa
joshyeom <jhyeom95@daum.net>
2024-08-13 15:35:40 +0900

    refactor: mypage 스타일링 및 추가 로직 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,18 @@
 import { db } from "../firebase";
-import { doc , getDoc} from "firebase/firestore";
+import { doc , getDoc, deleteDoc, updateDoc} from "firebase/firestore";
 import { useAuth } from "../contexts/AuthContext";
 import { useEffect, useState } from "react";
 import { useRouteHandler } from "../hooks/useRouteHandler";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
+
 
 interface UserDataType {
   id: string;
@@ -11,12 +21,15 @@ interface UserDataType {
   imageUrls: string[];
   price: string;
   title: string;
+  userId: string;
 }
+
 
 const MyPage = () => {
   const { currentUser } = useAuth();
   const [userData, setUserData] = useState<UserDataType[]>([]);
   const route = useRouteHandler()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -29,8 +42,7 @@ const MyPage = () => {
                 if (userDocSnap.exists()) {
                  
                     const userData = userDocSnap.data();
-                    const postsIdArray = userData.postsId; 
-                    console.log(userData)
+                    const postsIdArray = userData.postsId;
 
                     if (postsIdArray && postsIdArray.length > 0) {
                         const postFetches = postsIdArray.map(async (postId: string) => {
@@ -61,26 +73,76 @@ const MyPage = () => {
     fetchUserData();
   }, [currentUser]);
 
+
+
+    const deletePost = async (postId: string, uid: string) => {
+      try {
+          const postDocRef = doc(db, 'allPosts', postId);
+          await deleteDoc(postDocRef);
+
+          const userPostsRef = doc(db, "userPosts", uid);
+          const docSnap = await getDoc(userPostsRef);
+          const snapData = docSnap.data();
+
+          if(!snapData){
+            return
+          }
+
+          const snapDataPostsId = snapData.postsId || [];
+          const filteredData = snapDataPostsId.filter((id: string) => id !== postId)
+          await updateDoc(userPostsRef, {
+            postsId: filteredData,
+          });
+
+          console.log(`Post with ID ${postId} deleted successfully.`);
+      } catch (error) {
+          console.error("Error deleting post:", error);
+      }
+    };
+
+    const deleteHandler = (postId: string, uid: string) => {
+      const confirmed = confirm("삭제 하시겠습니까?")
+      if(confirmed){
+        deletePost(postId, uid)
+        alert("삭제 성공")
+      }
+      else{
+        return
+      }
+    }
+
+    const editHandler = (post: UserDataType) => {
+      navigate(`/create`, { state: { post } })
+    }
+
+
+
   return (
-    <div>
-      {userData.length > 0 ? (
-        userData.map((data, index) => (
-          <div key={index} onClick={() => route(`detail/${data.id}`)}>
-            <span>{data.email}님의 페이지</span>
-            <span>{data.title}</span>
-            <p>{data.description}</p>
-            <p>{data.price}</p>
-            {/* <div> */}
-              {/* {data.imageUrls.map((image, i) => ( 
-               <img key={i} src={image} alt={`image-${i}`} />
-             ))} */}
-           {/* </div> */}
-          </div>
-        ))
-      ) : (
-        <div>등록된 게시글이 없습니다.</div>
-      )}
-    </div>
+    <main>
+      <h2>등록된 물품 정보</h2>
+      <h2>품목</h2>
+      <section className="flex flex-col items-center justify-center min-h-screen">
+        <section className="w-[1100px] flex flex-wrap gap-[20px]">
+          {userData.length > 0 ? (
+            userData.map((data) => (
+              <Card className="w-[250px]"  key={data.title}>
+                <img src={data.imageUrls[0]} alt={data.imageUrls[0]} style={{ width: "100%",height: "200px" ,objectFit: "cover"}}  onClick={() => route(`detail/${data.id}`)}/>
+                <CardHeader>
+                  <CardTitle>{data.title}</CardTitle>
+                  <p>{data.price}원</p>
+                </CardHeader>
+                <CardContent className="flex items-center justify-between">
+                  <Button onClick={() => editHandler(data)}>수정</Button>
+                  <Button onClick={() => deleteHandler(data.id, data.userId)}>삭제</Button>
+                </CardContent>
+              </Card>
+            ))
+          ) : (
+            <div>등록된 게시글이 없습니다.</div>
+          )}
+        </section>
+      </section>
+    </main>
   );
 };
 
