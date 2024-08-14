fa08fbd0fe7dfbede641156f497b6df10a8be654
joshyeom <jhyeom95@daum.net>
2024-08-14 20:49:37 +0900

    refactor: Main page 버튼 chadcn/ui 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -19,25 +19,22 @@ const Main = () => {
 
 
   return (
-    <>
+    <div>
       {!currentUser ? (
         <>
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
           <Button onClick={() => route('signin')}>로그인</Button>
-          <button onClick={() => route('signup')}>회원가입</button>
+          <Button onClick={() => route('signup')}>회원가입</Button>
         </>
       ) : (
         <>
             <div>환영합니다</div>
-            <button onClick={signoutHandler}>로그아웃</button>
-            <button onClick={() => route('mypage')}>마이페이지</button>
-            <button onClick={() => route('create')}>게시글 생성</button>
+            <Button onClick={signoutHandler}>로그아웃</Button>
+            <Button onClick={() => route('mypage')}>마이페이지</Button>
+            <Button onClick={() => route('create')}>게시글 생성</Button>
         </>
-      )
-    }
-    </>
+        )
+      }
+    </div>
   );
 }
 
