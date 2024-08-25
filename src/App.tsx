8c1e2c805905f25a982d1e7e431a63a6fe78d862
joshyeom <jhyeom95@daum.net>
2024-08-25 17:01:49 +0900

    feat: react-helmet 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main, Signin, Signup, CreatePost, MyPosts, DetailPost, EditPost, MyLike, Category } from "./pages";
 import { AuthProvider } from "./contexts/AuthContext";
 import './index.css'
+import { Helmet } from 'react-helmet'
 
 
 const App = () => {
   return(
     <AuthProvider>
       <Router>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Businsa</title>
+            <meta name="description" content="Welcome to Businsa" />
+          </Helmet>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signin" element={<Signin />} />
