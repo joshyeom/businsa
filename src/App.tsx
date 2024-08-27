d0dd3bc38f41195568d68dd35bd49e2e7c35e47b
joshyeom <jhyeom95@daum.net>
2024-08-27 16:19:34 +0900

    feat: Tanstack-query 설치 및 QueryProvider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,37 @@ import { Main, Signin, Signup, CreatePost, MyPosts, DetailPost, EditPost, MyLike
 import { AuthProvider } from "./contexts/AuthContext";
 import './index.css'
 import { Helmet } from 'react-helmet'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return(
+  return (
     <AuthProvider>
-      <Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
           <Helmet>
             <meta charSet="utf-8" />
             <title>Businsa</title>
             <meta name="description" content="Welcome to Businsa" />
           </Helmet>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/edit" element={<EditPost />} />
-          <Route path="/myposts" element={<MyPosts />} />
-          <Route path="/myLike" element={<MyLike />} />
-          <Route path="/detail/:id" element={<DetailPost />} />
-          <Route path="/:id" element={<Category />} />
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/edit" element={<EditPost />} />
+            <Route path="/myposts" element={<MyPosts />} />
+            <Route path="/myLike" element={<MyLike />} />
+            <Route path="/detail/:id" element={<DetailPost />} />
+            <Route path="/:id" element={<Category />} />
+          </Routes>
+        </Router>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </AuthProvider>
-  )
+  );
 }
 
 export default App
